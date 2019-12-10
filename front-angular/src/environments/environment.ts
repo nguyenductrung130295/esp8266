0edf29965b934b6f1e2d59233376a4ba0fd71cbb
trung <trungnguyencntt1302@gmail.com>
2019-12-11 01:05:11 +0700

    add firebase

--- front-angular/src/environments/environment.ts
+++ front-angular/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCprTnZxw080vBr221RTk2m7IpJl7cUMhU",
+    authDomain: "front-angular-76558.firebaseapp.com",
+    databaseURL: "https://front-angular-76558.firebaseio.com",
+    projectId: "front-angular-76558",
+    storageBucket: "front-angular-76558.appspot.com",
+    messagingSenderId: "360848844868",
+    appId: "1:360848844868:web:4a7ae63e18d7c3294eb411",
+    measurementId: "G-HCBBFHWN2V"
+  }
 };
 
 /*
