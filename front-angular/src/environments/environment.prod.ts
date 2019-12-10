760d052446f034cac21c1d8017a4564e572d4f52
trung <trungnguyencntt1302@gmail.com>
2019-12-11 01:41:34 +0700

    deploytofirebasehosting

--- front-angular/src/environments/environment.prod.ts
+++ front-angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCprTnZxw080vBr221RTk2m7IpJl7cUMhU",
+    authDomain: "front-angular-76558.firebaseapp.com",
+    databaseURL: "https://front-angular-76558.firebaseio.com",
+    projectId: "front-angular-76558",
+    storageBucket: "front-angular-76558.appspot.com",
+    messagingSenderId: "360848844868",
+    appId: "1:360848844868:web:4a7ae63e18d7c3294eb411",
+    measurementId: "G-HCBBFHWN2V"
+  }
 };
